3992381349f3d268a7153ae2a31762919c40cd4b
JerryCodesAfrica <egemonyeemelie@gmail.com>
2024-04-21 14:15:36 +0100

    additional features

--- script.sh
+++ script.sh
@@ -1,51 +1,35 @@
-# First we want to know the contents of the storage file
+# First, we want to know the contents of the storage file
 function clearfolder() {
-    rm -r download
-
+    rm -ri download
 }
 
 menu() {
-echo "Before executing the script would you like to empty the downloads file"
-read answer
-echo " "
-case $answer in
-    "1")
-        clearfolder
-    ;;
-    "2")
-        echo "Proceed"
+    read -p "Do you want to clear the downloads folder? (1 for yes, 2 for no) " answer
+    answer=${answer:-2}
+    echo " "
+    if [[ "$answer" == "1" ]] ; then
+        clearfolder;
+    else
+        echo "You can proceed to downl"
+    fi
 }
 
 
-
-
-
-
- 
 function list() {
-echo "Here are the files in your s3 bucket"
-aws s3 ls s3://clouduploadertds
-echo " "
-# Collecting user input on choice of file to download
-read -p "What would you like to download " file
+    echo "Here are the files in your s3 bucket"
+    aws s3 ls --human-readable s3://clouduploadertds
+    echo " "
+    # Collecting user input on choice of file to download
+    local file
+    read -p "What would you like to download " file
+    # Downloading the selected file
+    mkdir -p download && if aws s3 cp s3://clouduploadertds/$file download/$file; then
+        echo "File downloaded successfully!"
+    else
+        echo "Download failed. Check your internet connectivity and try again."
+    fi
 }
 
-# Calling the function to get the list of items in the cloud storage
+# error handling for the download
 
-
-
-
-# function to download file from the cloud onto our local device
-
-function download() {
-    aws s3 cp s3://clouduploadertds/${file} downloads
-}
-
-# validation to check if the file already exists in the downloads folder
-
-
- # function to clear the download folder on demand
-
-
-
- 
+menu
